9389724d1e14ed142e5516bca2a2e299c5cdb190
MichaelJDaniel <soldbymichaeld@gmail.com>
2018-11-13 08:45:45 -0700

    add note/notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/show'
+  get 'notes/edit'
+  get 'notes/new'
+  resources :notes
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
